d7534bfae45588e920e8e1217407de7f378a7256
cimimorio <cimimorio@xxoo.local>
2017-02-08 23:16:13 +0800

    add some interface

--- MeChat/Controllers/ViewController.swift
+++ MeChat/Controllers/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var accountLabel: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
-
+    @IBAction func loginBtnClicked(_ sender: Any) {
+        IMChatManager.share().mLogin(withAccount: accountLabel.text, andToken: passwordLabel.text) { [weak self](error) in
+            if error != nil{
+                print(error ?? "ssss")
+            }else{
+                print("no error")
+                let mainSB = UIStoryboard.init(name: "Main", bundle: Bundle.main)
+                guard let chatVC:ChatViewController = mainSB.instantiateViewController(withIdentifier: "ChatViewController") as? ChatViewController else{
+                    return
+                }
+                self?.present(chatVC, animated: true, completion: { 
+                    
+                })
+            }
+        }
+    }
 }
 
